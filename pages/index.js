5c617160ed243458b7f093473b19ecac7c3889b4
Carls13 <charlesshb98@gmail.com>
2019-06-26 23:23:40 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import 'isomorphic-fetch'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import ChannelGrid from '../components/ChannelGrid'
+
+export default class extends React.Component{
+
+	static async getInitialProps(){
+		let req = await fetch('https://api.audioboom.com/channels/recommended')
+
+		let { body: channels } = await req.json();
+
+		return {
+			channels
+		}
+	}
+	render(){
+		const { channels } = this.props
+
+		return(
+		<Layout title="Podcasts">
+			<ChannelGrid channels={channels}/>			
+		
+		
+		</Layout>)
+	}
+}
